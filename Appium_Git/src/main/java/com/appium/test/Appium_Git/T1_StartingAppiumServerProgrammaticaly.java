93cc18af807eeb8730a7f72a43b555926b9f989b
PrasadMadge <PrasadMadge@DESKTOP-LARSM5G>
2018-06-03 17:20:39 +0530

    Appium server start and stop programmatically using java

--- Appium_Git/src/main/java/com/appium/test/Appium_Git/T1_StartingAppiumServerProgrammaticaly.java
+++ Appium_Git/src/main/java/com/appium/test/Appium_Git/T1_StartingAppiumServerProgrammaticaly.java
@@ -1,13 +1,60 @@
 package com.appium.test.Appium_Git;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+
+public class T1_StartingAppiumServerProgrammaticaly {
+	
+	//Install appium and node (node >8 , appium = 1.8 version)
+	// Update your node and appium main.js path
+	
+	static String nodePath="C:\\Program Files (x86)\\nodejs\\node.exe";
+	static String appiumMainJsPath = "C:\\Users\\PrasadMadge\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+	static int portNo=4723;
+	
+	
+	public static void main(String[] args) {
+		
+		
+		AppiumDriverLocalService service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+				.usingDriverExecutable(new File(nodePath))
+				.withAppiumJS(new File(appiumMainJsPath))
+				.withIPAddress("0.0.0.0").usingPort(portNo));
+		
+		System.out.println("Appium server Starting");
+		service.start();
+		System.out.println("Appium server started");
+		
+		
+		
+		if(checkIfServerIsRunnning(portNo)) {
+			service.stop();
+			System.out.println("Appium server stopped");
+		} else {
+			System.out.println("Appium server not started with port" + portNo);
+		}
+		
+
+	}
+
+	public static boolean checkIfServerIsRunnning(int port) {
+
+		boolean isServerRunning = false;
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(port);
+			serverSocket.close();
+		} catch (IOException e) {
+			// If control comes here, then it means that the port is in use
+			isServerRunning = true;
+		} finally {
+			serverSocket = null;
+		}
+		return isServerRunning;
+	}
+
 }
