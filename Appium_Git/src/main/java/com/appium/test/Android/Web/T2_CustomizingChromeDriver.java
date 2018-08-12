3a7bd3dba5ffaf27d0de9a027f45cd10bdd88f7c
Prasad Madge <prasadmadge8@gmail.com>
2018-08-12 16:49:50 +0530

    Small corrections and new class added

--- Appium_Git/src/main/java/com/appium/test/Android/Web/T2_CustomizingChromeDriver.java
+++ Appium_Git/src/main/java/com/appium/test/Android/Web/T2_CustomizingChromeDriver.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
@@ -15,11 +14,11 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-public class MyFirstWebTestChromeBrowser {
+public class T2_CustomizingChromeDriver {
 
 	
 	//***************** PreConditions
-	// APK is already installed
+	// Chrome browser needs to be installed
 	// Connect android device via USB with adb enabled
 	// Install appium and node (node >8 , appium = 1.8 version)
 	// Update your node and appium main.js path
@@ -33,23 +32,29 @@ public class MyFirstWebTestChromeBrowser {
 
 		// Note
 		
-		/*locating web elements is very simple as it is similar to selenium
-		Open your chrome browser in desktop , turn on developer options
-		Since web applications are reponsive, turn on mobile view for inspecting elements
-		the same locators are used in appium scripts for automating web apps
+		/*
+		 * If may happen that your chrome browser version is not matching with the chrome driver in appium default path
+		 * double click chromedriver.exe and you will come to know its version 
+		 * Path : C:\Users\USER\AppData\Roaming\npm\node_modules\appium\node_modules\appium-chromedriver\chromedriver\win\chromedriver.exe
+		 * 
+		 * Hence there are 2 solutions to it
+		 * 1 : downgrade your chrome browser version matching with chromedriver
+		 * 2 : update chromedriver in default path on appium ,
+		 * Path : C:\Users\USER\AppData\Roaming\npm\node_modules\appium\node_modules\appium-chromedriver\chromedriver\win\chromedriver.exe
+		 * 
+		 * But both the solutions are not good , why?
+		 * what if you are doing parallel execution , with different chrome browser versions.
+		 * What if one android phone is getting chrome browser update till specific version
+		 * 
+		 * You need to point different chromedriver to according to chrome browser installed on your phone
+		 * Appium allows us to do this with one additional capability mentioned below
+		 * chromedriverExecutable --> path to new chromedriver.exe
+		 * 
+		 * More Info : http://appium.io/docs/en/writing-running-appium/web/chromedriver/
 		
-		Tips
-		If automating and Web app always use By object
-		
-		Please check that the chromedriver of appium is compatible with the chrome browser on your phone
-		More Info : http://appium.io/docs/en/writing-running-appium/web/chromedriver/
-		You can download chrome driver and copy paste it in chromedriver folder of appium
-		
+		 * 
 		*/
 		
-		
-		
-	
 		
 		AppiumDriverLocalService service = AppiumDriverLocalService
 				.buildService(new AppiumServiceBuilder().usingDriverExecutable(new File(nodePath))
@@ -68,9 +73,10 @@ public class MyFirstWebTestChromeBrowser {
 		caps.setCapability(MobileCapabilityType.NO_RESET, true);
 		caps.setCapability(MobileCapabilityType.UDID, "ZY32287RPN");
 
-		// capabilities for apk
+		// capabilities for web
 		caps.setCapability(AndroidMobileCapabilityType.BROWSER_NAME, BrowserType.CHROME);
-
+		
+		caps.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, "D:\\DriversMobile\\chromedriver37\\chromedriver.exe");
 		try {
 			// App will launch if everything goes fine
 			driver = new AndroidDriver<MobileElement>(service, caps);
